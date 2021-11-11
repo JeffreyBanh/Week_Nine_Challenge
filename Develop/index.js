d791b74ce5dd7ecde648b81315310a76346e0343
JeffreyBanh <banh.jeff@gmail.com>
2021-11-10 23:42:50 -0800

    add objects

--- Develop/index.js
+++ Develop/index.js
@@ -1,8 +1,57 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const util = require("util");
+const generatorMarkdown = require('./util/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const questions = [];
-
+inquirer.prompt([
+    {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?'
+    },
+    {
+    type: 'input',
+    name: 'description',
+    message: 'Add a description to the project'
+    },
+    {
+    type: 'input',
+    name: 'Installation',
+    message: 'Add a description to the project'
+    },
+    {
+    type: 'input',
+    name: 'Usage',
+    message: 'Add a description to the project'
+    },
+    {
+    type: 'input',
+    name: 'Credits',
+    message: 'Add a description to the project'
+    },
+    {
+    type: 'input',
+    name: 'License',
+    message: 'Add a description to the project'
+    },
+    {
+    type: 'input',
+    name: 'Badges',
+    message: 'Add a description to the project'
+    },
+    {
+    type: 'input',
+    name: 'Contributing',
+    message: 'Add a description to the project'
+    },
+    {
+    type: 'input',
+    name: 'Test',
+    message: 'Add a description to the project'
+    },
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
